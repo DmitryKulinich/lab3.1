d120d1286853fed801bf0bc44128adcc73d657e4
Dmitry <elegiacal.boy@gmail.com>
2018-01-16 00:20:38 +0200

    with facade, singleton, lazy initialization and factory method +XSD

--- src/model/ReadWriteXML.java
+++ src/model/ReadWriteXML.java
@@ -1,11 +1,16 @@
 package model;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.*;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.util.JAXBSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by dmitry on 9/19/17.
@@ -32,23 +37,46 @@ public class ReadWriteXML extends metroStation implements ReadWrite{
             File file = new File(title);
             JAXBContext jaxbContext = JAXBContext.newInstance(ReadWriteXML.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-
+            SchemaFactory sf = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
+            Schema schema = null;
+            try {
+                schema = sf.newSchema(new File("src/schemaXsd/schema.xsd"));
+            } catch( SAXException e){
+                e.printStackTrace();
+            }
+            JAXBSource source = new JAXBSource(jaxbContext, this);
+            Validator validator = schema.newValidator();
+            validator.setErrorHandler(new CustomValidationErrorHandler());
+            try {
+                validator.validate(source);
+            } catch( SAXException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            jaxbMarshaller.setSchema(schema);
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-
             jaxbMarshaller.marshal(this, file);
-
         } catch (JAXBException e) {
             e.printStackTrace();
         }
     }
 
+
     public ReadWriteXML read(File file){
         ReadWriteXML obj = new ReadWriteXML();
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(ReadWriteXML.class);
-
+            SchemaFactory sf = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
+            Schema schema = null;
+            try {
+                schema = sf.newSchema(new File("src/schemaXsd/schema.xsd"));
+            } catch( SAXException e){
+                e.printStackTrace();
+            }
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            jaxbUnmarshaller.setSchema(schema);
+            jaxbUnmarshaller.setEventHandler(new CustomValidationEventHandler());
             obj = (ReadWriteXML) jaxbUnmarshaller.unmarshal(file);
 
         } catch (JAXBException e) {
